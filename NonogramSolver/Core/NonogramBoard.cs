1ef454f96ccb41679527615384a0a690cc3e78b9
Ігор Мовчан <i.movchan@ukma.edu.ua>
2026-08-15 17:04:50 +0300

    Add nonogram board model

---
+++ NonogramSolver/Core/NonogramBoard.cs
@@ -0,0 +1,11 @@
+﻿namespace Core
+{
+    public class NonogramBoard
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public List<List<int>> Rows { get; set; } = new();
+        public List<List<int>> Columns { get; set; } = new();
+    }
+}
